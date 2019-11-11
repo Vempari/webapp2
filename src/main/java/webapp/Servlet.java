7e0637fb166e13a93a937a5373a0009381663741
vempari <Vempari_2@mail.ru>
2019-11-11 21:50:19 +0300

    Initial commit

---
+++ src/main/java/webapp/Servlet.java
@@ -0,0 +1,18 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("Hello, servlet!");
+
+    }
+}
